5bcf07eb8432de567fb9f4023333ea0b1c917964
Jack <jack.woodhouse.97@gmail.com>
2019-01-10 13:32:27 +0000

    update

--- Assignment/Assignment.ino
+++ Assignment/Assignment.ino
@@ -44,18 +44,17 @@ NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and
 const int ledPin = 13; // the pin that the LED is attached to
 int incomingByte;      // a variable to read incoming serial data into
 
-boolean canMove;
+boolean canMove = true;
 
 //SENSORS
 
-unsigned int sensor_values[NUM_SENSORS];
+unsigned int sensor_values[6];
 ZumoReflectanceSensorArray sensors(QTR_NO_EMITTER_PIN);
 
 
 void setup() {
   // initialize serial communication:
   Serial.begin(9600);
-  canMove = true;
   reflectanceSensors.init();
 
 }
@@ -63,29 +62,23 @@ void setup() {
 void loop() {
 
 
-  while(canMove = true){  
-    
+  movement();
+
   borderDetect();
-  
+
+  while (canMove = true) {
+
+    sensors.read(sensor_values);
+
+
   }
 
   delay(250);
-  
-  //delay(250);                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
-  //  Serial.print("Ping: ");
-  //  Serial.print(sonar.ping_cm()); // Send ping, get distance in cm and print result (0 = outside set distance range)
-  //  Serial.println("cm");
 
-  //  if(sonar.ping() < MAX_DISTANCE){
-  //    Serial.print("CLOSE");
-  //    motors.setSpeeds(-100, -100);
-  //  }
-  //  else {
-  //    motors.setSpeeds(100,100);
-  //  }
+}
 
+void movement() {
 
-  // see if there's incoming serial data:
   if (Serial.available() > 0)
   {
     // read the oldest byte in the serial buffer:
@@ -113,7 +106,6 @@ void loop() {
 
 }
 
-
 void borderDetect()
 {
 
@@ -132,28 +124,48 @@ void borderDetect()
     canMove = false;
   }
   else {
-    //nothing yet
+    
   }
 
+  
+    if (sensor_values[0] > QTR_THRESHOLD)
+    {
 
-  if (sensor_values[0] > QTR_THRESHOLD)
-  {
+      motors.setSpeeds(150, 0);
+      delay(250);
+      motors.setSpeeds(50, 50);
+      delay(250);
 
-    motors.setSpeeds(100, 0);
-    delay(250);
-    motors.setSpeeds(50, 50);
+    }
+    else if (sensor_values[5] > QTR_THRESHOLD)
+    {
 
-  }
-  else if (sensor_values[5] > QTR_THRESHOLD)
-  {
+      motors.setSpeeds(0, 150);
+      delay(250);
+      motors.setSpeeds(50, 50);
+      delay(250);
 
-    motors.setSpeeds(0, 100);
-    delay(250);
-    motors.setSpeeds(50, 50);
-
-  }
-  else {
-    // nothing yet
-  }
+    }
+    else {
+      
+    }
 
 }
+
+
+
+//delay(250);                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+//  Serial.print("Ping: ");
+//  Serial.print(sonar.ping_cm()); // Send ping, get distance in cm and print result (0 = outside set distance range)
+//  Serial.println("cm");
+
+//  if(sonar.ping() < MAX_DISTANCE){
+//    Serial.print("CLOSE");
+//    motors.setSpeeds(-100, -100);
+//  }
+//  else {
+//    motors.setSpeeds(100,100);
+//  }
+
+
+// see if there's incoming serial data:
