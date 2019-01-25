eeb007f762e41e82125964c03e4720b715c82381
Jack <jack.woodhouse.97@gmail.com>
2019-01-25 16:24:01 +0000

    task 5 working as intended
    all tasks up to 5 are completed and the video is also completed

--- Assignment/Assignment.ino
+++ Assignment/Assignment.ino
@@ -3,18 +3,12 @@
 #include <ZumoShield.h>
 
 
-#define TRIGGER_PIN  2  // Arduino pin tied to trigger pin on the ultrasonic sensor.
-#define ECHO_PIN     6  // Arduino pin tied to echo pin on the ultrasonic sensor.
-#define MAX_DISTANCE 50 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
-
-#define FORWARD_SPEED 150
-#define REVERSE_SPEED -150
-#define STOP 0
+#define TRIGGER_PIN  6  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     2  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 30 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
 #define QTR_THRESHOLD 800 // microseconds
 #define NUM_SENSORS 6
-
-
 
 ZumoMotors motors;
 
@@ -24,14 +18,22 @@ Pushbutton button(ZUMO_BUTTON);
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
-bool anEnd = false;
 bool canMove = true;
 
-int Input;      // a variable to read incoming serial data into
+int endCount = 0;
+
+int Input; // a variable to read incoming serial data into
 
 unsigned int sensor_values[NUM_SENSORS];
 
 int roomNumber = 0; // integer variable to store the room number
+
+bool ignore = false;
+
+bool room = false;
+
+bool tJunction = false;
+
 
 String roomsAndLocations[10]; // an array to hold the rooms and locations
 String roomsWithObjects[10]; // an array to hold the rooms with object
@@ -43,9 +45,9 @@ void setup()
   Serial.begin(9600);
 
   // Initialize the reflectance sensors module
-  sensors.init();
+  sensors.init(QTR_NO_EMITTER_PIN);
 
-  sensors.calibrate();
+  sensors.calibrate(QTR_NO_EMITTER_PIN);
 
   button.waitForButton();
 }
@@ -54,34 +56,35 @@ void loop()
 {
 
   movement();
-
+  
   if (canMove == true) {
 
-    motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+    motors.setSpeeds(100, 100);
     borderDetect();
 
   }
+   
 }
 
 void movement()
 {
-
-  bool ignore = false;
-  bool passed = false;
-  bool tJunction = false;
-  int endCount = 0;
 
   if (Serial.available() > 0)
   {
     // read the oldest byte in the serial buffer:
     Input = Serial.read();
 
+    if (Input == 'w')
+    {
+      motors.setSpeeds(150, 150);
+    }
+
     if (Input == 's')
     {
 
       if (!ignore)
       {
-        motors.setSpeeds(REVERSE_SPEED, REVERSE_SPEED);
+        motors.setSpeeds(-150, -150);
       }
 
     }
@@ -91,49 +94,89 @@ void movement()
 
       if (!ignore)
       {
-        motors.setSpeeds(REVERSE_SPEED, FORWARD_SPEED);
+        motors.setSpeeds(-150, 150);
       }
       else
       {
+        Serial.println("ignoring commands!");
         tJunction = true;
+        ignore = false;
+        canMove = true;
       }
 
     }
 
     if (Input == 'd')
     {
-
       if (!ignore)
       {
-        motors.setSpeeds(FORWARD_SPEED, REVERSE_SPEED);
+        motors.setSpeeds(150, -150);
       }
       else
       {
+        Serial.println("ignoring commands!");
         tJunction = true;
+        ignore = false;
+        canMove = true;
       }
 
     }
-
-
 
     if (Input == 'x')
     {
-      if (!ignore)
-      {
-
-        motors.setSpeeds(STOP, STOP);
-      }
+      motors.setSpeeds(0,0);
     }
 
-    if (Input == 'c') {
+    if (Input == 'c')
+    {
       Serial.println("Action complete!");
       Serial.println("Resuming!");
       delay(250);
       canMove = true;
     }
 
-    if (Input == 'e') {
+    if (Input == 'r') {
 
+      if (!ignore)
+      {
+        Serial.println ("Entering a room on the right!");
+        delay(250);
+        motors.setSpeeds(150, -150);
+        delay(250);
+        motors.setSpeeds(0, 0);
+        searchRoom("right");
+      }
+      else {
+        ignore = false;
+        Serial.println("ignoring Commands!");
+        room = true;
+
+      }
+    }
+
+    if (Input == 'l') {
+
+      if (!ignore)
+      {
+        Serial.println ("Entering a room on the left!");
+        delay(250);
+        motors.setSpeeds(-150, 150);
+        delay(250);
+        motors.setSpeeds(0, 0);
+        searchRoom("left");
+      }
+      else {
+        
+        ignore = false;
+        Serial.println("ignoring Commands!");
+        room = true;
+      }
+
+    }
+
+    if (Input == 'e')
+    {
+      endCount++;
       Serial.println("You hit an end!");
 
       while (!canMove && (endCount != 2))
@@ -141,7 +184,7 @@ void movement()
         movement();
       }
 
-      if (!passed && !tJunction)
+      if (!room && !tJunction)
       {
         ignore = true;
       }
@@ -168,44 +211,7 @@ void movement()
             Serial.println(roomWithObject);
           }
         }
-
       }
-    }
-
-    if (Input == 'r') {
-
-      if (!ignore)
-      {
-
-        Serial.println ("Entering a room on the right!");
-        delay(250);
-        motors.setSpeeds(FORWARD_SPEED, REVERSE_SPEED);
-        delay(350);
-        motors.setSpeeds(STOP, STOP);
-        searchRoom("right");
-      }
-      else {
-
-        tJunction = true;
-
-      }
-    }
-
-    if (Input == 'l') {
-
-      if (!ignore)
-      {
-        Serial.println ("Entering a room on the left!");
-        delay(250);
-        motors.setSpeeds(REVERSE_SPEED, FORWARD_SPEED);
-        delay(350);
-        motors.setSpeeds(STOP, STOP);
-        searchRoom("left");
-      }
-      else {
-        tJunction = true;
-      }
-
     }
   }
 }
@@ -219,31 +225,31 @@ void borderDetect()
   {
 
     canMove = false;
-    motors.setSpeeds(STOP, STOP);
-
+    motors.setSpeeds(0, 0);
     Serial.println("You hit a wall!");
     Serial.println("Please enter a command");
-    motors.setSpeeds(-REVERSE_SPEED, -REVERSE_SPEED);
+    motors.setSpeeds(-150, -150);
     delay(200);
-    motors.setSpeeds(STOP, STOP);
+    motors.setSpeeds(0, 0);
     movement();
 
   }
   else
   {
     delay(50);
+    
     if (sensor_values[0] > QTR_THRESHOLD)
     {
-      motors.setSpeeds(STOP, STOP);
-      delay(250);
-      motors.setSpeeds(FORWARD_SPEED, -50);
+      motors.setSpeeds(0, 0);
+      delay(500);
+      motors.setSpeeds(150, -50);
       delay(250);
     }
     else if (sensor_values[5] > QTR_THRESHOLD)
     {
-      motors.setSpeeds(STOP, STOP);
-      delay(250);
-      motors.setSpeeds(-50, FORWARD_SPEED);
+      motors.setSpeeds(0, 0);
+      delay(500);
+      motors.setSpeeds(-50, 150);
       delay(250);
     }
   }
@@ -253,6 +259,7 @@ void borderDetect()
 void searchRoom(String location)
 {
 
+  canMove = false;
   bool found = false;
 
   roomNumber++; // increment the room number
@@ -265,34 +272,27 @@ void searchRoom(String location)
 
   Serial.println("Searching room");
 
-  delay(250);
 
-  motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
-
-  delay(250);
-
-  motors.setSpeeds(REVERSE_SPEED, FORWARD_SPEED);
-
-  delay(500);
-
-  motors.setSpeeds(STOP, STOP);
-
-  for (int i = 0; i < 100; i++)
+  for (int i = 0; i < 80; i++)
   {
-
-    if ((i > 40 && i <= 60) || (i > 80 && i <= 100)) // if i is greater than 40 and i is less than or equal to 60 or i is greater than 80 or less than or equal to 100
-      motors.setSpeeds(REVERSE_SPEED, FORWARD_SPEED);
+    if ((i > 10 && i <= 30) || (i > 50 && i <= 70)) // if i is greater than 40 and i is less than or equal to 60 or i is greater than 80 or less than or equal to 100
+    {
+      motors.setSpeeds(-150, 150);
+    }
     else
-      motors.setSpeeds(FORWARD_SPEED, REVERSE_SPEED); // set the left motor to a speed of 150 forwards and the right to 150 in reverse to turn the Zumo right
-
+    {
+      motors.setSpeeds(150, -150); // set the left motor to a speed of 150 forwards and the right to 150 in reverse to turn the Zumo right
+    }
+    
     if (sonar.ping_cm() > 0) // if the ultrasonic sensor has a reading in centimetres greater than 0
     {
       found = true;
-      motors.setSpeeds(STOP, STOP);
+      motors.setSpeeds(0, 0);
     }
     delay(50);
   }
-  motors.setSpeeds(STOP, STOP);
+  
+  motors.setSpeeds(0, 0);
 
   if (found)
   {
@@ -302,6 +302,7 @@ void searchRoom(String location)
   else {
     Serial.println("No object found in this room!");
   }
+  
 }
 
 void storeObjectDetected(int roomNumber)
